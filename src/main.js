26a69675c138fd3e8cdb1e25da5a9b7427a0b40a
Taalai <t2.kg@mail.ru>
2021-02-24 03:29:02 +0600

    'initial'

---
+++ src/main.js
@@ -0,0 +1,236 @@
+import React, {useState} from 'react';
+import krao from "./images/photo-1588618319407-948d4424befd.jpg";
+
+import uk01 from '../src/images/image-ustav/charter.jpg'
+import uk1 from '../src/images/image-ustav/11-001.jpg'
+import uk2 from '../src/images/image-ustav/12-001.jpg'
+import uk3 from '../src/images/image-ustav/11-002.jpg'
+import uk4 from '../src/images/image-ustav/11-003.jpg'
+import uk5 from '../src/images/image-ustav/11-004.jpg'
+import uk6 from '../src/images/image-ustav/11-005.jpg'
+import uk7 from '../src/images/image-ustav/11-006.jpg'
+import uk8 from '../src/images/image-ustav/11-007.jpg'
+import uk9 from '../src/images/image-ustav/11-008.jpg'
+import uk10 from '../src/images/image-ustav/11-009.jpg'
+import uk11 from '../src/images/image-ustav/11-010.jpg'
+import uk12 from '../src/images/image-ustav/11-011.jpg'
+import uk13 from '../src/images/image-ustav/11-012.jpg'
+import uk14 from '../src/images/image-ustav/11-013.jpg'
+import uk15 from '../src/images/image-ustav/11-014.jpg'
+import uk16 from '../src/images/image-ustav/11-015.jpg'
+import uk17 from '../src/images/image-ustav/11-016.jpg'
+import uk18 from '../src/images/image-ustav/11-017.jpg'
+import uk19 from '../src/images/image-ustav/11-018.jpg'
+import uk20 from '../src/images/image-ustav/11-019.jpg'
+import uk21 from '../src/images/image-ustav/11-020.jpg'
+import uk22 from '../src/images/image-ustav/11-021.jpg'
+import uk23 from '../src/images/image-ustav/11-022.jpg'
+import uk24 from '../src/images/image-ustav/11-023.jpg'
+import uk25 from '../src/images/image-ustav/11-024.jpg'
+import uk26 from '../src/images/image-ustav/11-025.jpg'
+import uk27 from '../src/images/image-ustav/11-026.jpg'
+import educational_programs from '../src/images/photo-1596495578066-1aac1b785b3b.jpg'
+import strategic_plan_img from '../src/images/photo-1532294220147-279399e4e00f.jpg'
+import position_img from '../src/images/photo-1565688420536-11a4ddfa246f.jpg'
+import accreditation_img from '../src/images/photo-1558698972-c50e325e6799.jpg'
+import lk01 from '../src/images/image-license/depositphotos_85448338-stock-photo-licenses-on-business-folder-in.jpg'
+import lk1 from '../src/images/image-license/1-001-e1541832348523.jpg'
+import lk2 from '../src/images/image-license/001.jpg'
+import lk3 from '../src/images/image-license/002.jpg'
+import lk4 from '../src/images/image-license/003.jpg'
+import lk5 from '../src/images/image-license/004.jpg'
+import lk6 from '../src/images/image-license/005.jpg'
+import lk7 from '../src/images/image-license/006-e1541832401882.jpg'
+import lk8 from '../src/images/image-license/007-e1541832483862.jpg'
+import lk9 from '../src/images/image-license/008-e1541832471958.jpg'
+import lk10 from '../src/images/image-license/009-e1541832461998.jpg'
+import lk11 from '../src/images/image-license/010-e1541832450617.jpg'
+import lk12 from '../src/images/image-license/011-e1541832436155.jpg'
+import lk13 from '../src/images/image-license/012-e1541832427724.jpg'
+
+
+const Main = () => {
+    const [open, setOpen] = useState('hidden')
+    return (
+        <div >
+            <div className="mag">
+                <div className="img1" >
+                    <img src={krao} alt=""/>
+                </div>
+                <div className="img-bg"></div>
+                <div className="section">
+                    <p className="section1"> магистратура</p>
+                </div>
+            </div>
+            <div className="charter" style={{display: "flex"}}>
+                <div className="charter1">
+                  <h1>Устав КРАО 2017</h1>
+
+                </div>
+                <div  className="charter2">
+                    <img src={uk01} alt=""/>
+                    <img src={uk1} alt=""/>
+                    <img src={uk2} alt=""/>
+                    <img src={uk3} alt=""/>
+                    <img src={uk4} alt=""/>
+                    <img src={uk5} alt=""/>
+                    <img src={uk6} alt=""/>
+                    <img src={uk7} alt=""/>
+                    <img src={uk8} alt=""/>
+                    <img src={uk9} alt=""/>
+                    <img src={uk10} alt=""/>
+                    <img src={uk11} alt=""/>
+                    <img src={uk12} alt=""/>
+                    <img src={uk13} alt=""/>
+                    <img src={uk14} alt=""/>
+                    <img src={uk15} alt=""/>
+                    <img src={uk16} alt=""/>
+                    <img src={uk17} alt=""/>
+                    <img src={uk18} alt=""/>
+                    <img src={uk19} alt=""/>
+                    <img src={uk20} alt=""/>
+                    <img src={uk21} alt=""/>
+                    <img src={uk22} alt=""/>
+                    <img src={uk23} alt=""/>
+                    <img src={uk24} alt=""/>
+                    <img src={uk25} alt=""/>
+                    <img src={uk26} alt=""/>
+                    <img src={uk27} alt=""/>
+                </div>
+
+            </div>
+            <div className="educational_programs " style={{display: "flex"}}>
+                <div className="educational_programs1">
+                    <img src={educational_programs} alt=""/>
+                </div>
+                <div className="educational_programs2">
+                    <h1>Образовательные программы</h1>
+                    <h2>Образовательная программа высшего образования по направлению подготовки 530300 Психология (магистратура)</h2>
+                    <a href="http://krao.kg/wp-content/uploads/2018/11/jpg2pdf-2.pdf">
+                        Узнать больше
+                    </a>
+                </div>
+            </div>
+            <div className="strategic_plan" style={{display: "flex"}}>
+                <div className="strategic_plan1 ">
+                    <h1>Стратегический план</h1>
+                   <div>
+                       <h2>
+                           Стратегический план
+                       </h2>
+                       <a href="http://krao.kg/wp-content/uploads/2018/11/str.plan-KRAO-converted.pdf">
+                           Узнать больше
+                       </a>
+                   </div>
+                    <div>
+                       <h2>
+                           Политика обеспечения качества образования КРАО
+                       </h2>
+                        <a href="http://krao.kg/wp-content/uploads/2018/11/jpg2pdf.pdf">
+                            Узнать больше
+                        </a>
+                   </div>
+                </div>
+                <div className="strategic_plan2">
+                    <img src={strategic_plan_img} alt=""/>
+                </div>
+
+            </div>
+            <div className="license" style={{display: "flex"}}>
+                <div  className="license2">
+                    <img src={lk01} alt=""/>
+                    <img src={lk1} alt=""/>
+                    <img src={lk2} alt=""/>
+                    <img src={lk3} alt=""/>
+                    <img src={lk4} alt=""/>
+                    <img src={lk5} alt=""/>
+                    <img src={lk6} alt=""/>
+                    <img src={lk7} alt=""/>
+                    <img src={lk8} alt=""/>
+                    <img src={lk9} alt=""/>
+                    <img src={lk10} alt=""/>
+                    <img src={lk11} alt=""/>
+                    <img src={lk12} alt=""/>
+                    <img src={lk13} alt=""/>
+                </div>
+                <div className="license1">
+                    <h1>Лицензия КРАО 2017</h1>
+
+                </div>
+
+
+            </div>
+            <div className="position" style={{display: "flex"}}>
+                <div className="position2">
+                    <h1>Положение</h1>
+                    <div>
+                        <a href="http://krao.kg/wp-content/uploads/2018/11/File-1.pdf">
+                            Положение о проведении текущего контроля и промежуточной аттестации студентов НОУ ВПО КРАО
+                        </a>
+                    </div>
+                    <div>
+                        <a href="http://krao.kg/wp-content/uploads/2018/11/File0001-1.pdf">
+                            Положение о выпускных квалификационных работах бакалавра, дипломированного специалиста, магистра в КРАО
+                        </a>
+                    </div>
+                    <div>
+                        <a href="http://krao.kg/wp-content/uploads/2018/11/File0002.pdf">
+                            Положение об организации учебного процесса в КРАО с использованием модульно-рейтинговой технологии обучения
+                        </a>
+                    </div>
+                    <div>
+                        <a href="http://krao.kg/wp-content/uploads/2018/11/File0003.pdf">
+                            Положение о кафедре Психологии КРАО
+                        </a>
+                    </div>
+                    <div>
+                        <a href="http://krao.kg/wp-content/uploads/2018/11/File0004.pdf">
+                            Положение о кафедре Информационных технологии и Естественных технологических наук КРАО
+                        </a>
+                    </div>
+                    <div>
+                        <a href="http://krao.kg/wp-content/uploads/2018/11/File0005.pdf">
+                            Положение об итоговой государственной аттестации выпускников НОУ ВПО КРАО
+                        </a>
+                    </div>
+                    <div>
+                        <a href="http://krao.kg/wp-content/uploads/2018/11/jpg2pdf-1.pdf">
+                            Положение об организации учебного процесса в КРАО с использованием модульно-рейтинговой технологий обучения
+                        </a>
+                    </div>
+                </div>
+                <div className="position1">
+                    <img src={position_img} alt=""/>
+                </div>
+
+            </div>
+            <div className="accreditation" style={{display: "flex"}}>
+
+                <div className="accreditation2">
+                    <img src={accreditation_img} alt=""/>
+                </div>
+                <div className="accreditation1">
+                    <h1>Аккредитация</h1>
+                    <div>
+                        <div>
+                            <h2>
+                                СЕРТИФИКАТ № VU180000176
+                            </h2>
+                            <a href="http://krao.kg/wp-content/uploads/2021/02/SERTIFIKAT-N-VU180000176---kopiya-2.pdf">
+                                Узнать больше
+                            </a>
+                        </div>
+                    </div>
+                    <div className="accreditation3">
+                        <a href="http://www.aaopo.kg/">
+                            http://www.aaopo.kg/
+                        </a>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    );
+};
+
+export default Main;
